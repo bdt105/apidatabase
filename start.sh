b8d4ef371fc8ea87865440ad051385f7fff96840
bdt105@gmail.com <bdt105@gmail.com>
2018-07-31 20:24:59 +0100

    .

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-ps -ef | grep 'serverApiDatabase.js' | grep -v grep | awk '{print $2}' | xargs  1> killLog.out 2> killErr.out
+ps -ef | grep 'serverApiDatabase.js' | grep -v grep | awk '{print $2}' | xargs kill -9 1> killLog.out 2> killErr.out
 cd ./build 
 node serverApiDatabase.js 1> log.out 2> err.out &
